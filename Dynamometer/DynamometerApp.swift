f59241bfa10417e434056b74674868ffa868351b
Amir Latypov <Amir.Latypov@gmail.com>
2025-08-09 12:54:07 +0300

    Initial Commit

---
+++ Dynamometer/DynamometerApp.swift
@@ -0,0 +1,32 @@
+//
+//  DynamometerApp.swift
+//  Dynamometer
+//
+//  Created by Amir on 09.08.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DynamometerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
